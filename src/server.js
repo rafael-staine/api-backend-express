cd8ce80c3e25037822a37ae8b405e1e3e7e69722
rafael.staine@aluno.ifsp.edu.br <rafael.staine@aluno.ifsp.edu.br>
2025-08-12 17:48:43 -0300

    Cria o CRUD do profile

--- src/server.js
+++ src/server.js
@@ -1,12 +1,41 @@
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = 3333
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/profile', (req, res) => {
+  res.json({ message: 'Perfil de usuario!' })
+})
+
+app.post('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados)
+  res.json({
+    menssage: 'Usuário criado com sucesso!',
+    profile: dados
+  })
+})
+
+app.put('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados)
+  res.json({
+    menssage: 'Usuário editado com sucesso!',
+    profile: dados
+  })
+})
+
+app.delete('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados)
+  res.json({
+    menssage: 'Usuário deletado com sucesso!',
+    profile: dados
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port https://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
