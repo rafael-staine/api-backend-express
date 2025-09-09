59fd738d61b75d09b6b82d66ee3216a84db4609e
rafael.staine@aluno.ifsp.edu.br <rafael.staine@aluno.ifsp.edu.br>
2025-09-09 16:55:28 -0300

    Finalizando crud do profile

--- src/server.js
+++ src/server.js
@@ -8,9 +8,8 @@ import cors from 'cors'
 const app = express()
 const port = 3333
 
-app.use(express.json()) // Converter o JSON que chegou na requisição em um objeto js e vai salvar em req.body
-
 app.use(cors())
+app.use(express.json()) // Converter o JSON que chegou na requisição em um objeto js e vai salvar em req.body
 
 app.use('/profile', profileRouter)
 app.use('/product', productRouter)
